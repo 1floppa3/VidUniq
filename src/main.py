d9dd04650b5a1e659baec1a41481a8e105d995ce
Denis Kochetkov <denis.kochetkov2006@gmail.com>
2023-04-03 19:58:42 +0300

    Rewrite using my library VidUniq Change code logic (using argparse)

--- src/main.py
+++ src/main.py
@@ -1,23 +1,48 @@
-from VideoUniquelizer import VideoUniquelizer
-from utils import is_url
+import argparse
+
+from VidUniq import VideoUniquelizer
+from utils import is_url, Videos
+
+DEFAULT_DESTINATION_DIR = 'uniquelized/'
+DEFAULT_SOURCE_FILE = ''
 
 
 def main():
-    try:
-        path_list = []
-        url_list = []
-        with open('unique_list.txt') as file:
-            lines = filter(None, map(str.strip, file))
-            for line in lines:
-                if is_url(line):
-                    url_list.append(line)
-                else:
-                    path_list.append(line)
+    parser = argparse.ArgumentParser(description='Video Uniquelizer')
+    parser.add_argument('-d', '--destination_dir', type=str, default=DEFAULT_DESTINATION_DIR,
+                        help='Destination folder to store uniquelized videos')
+    parser.add_argument('-s', '--source_file', type=str, default=DEFAULT_SOURCE_FILE,
+                        help='Source text file with videos to unique')
+    parser.add_argument('videos', type=str, nargs='*',
+                        help='List of paths or links')
+    args = parser.parse_args()
 
-        uniq = VideoUniquelizer(path=path_list, url=url_list)
-        uniq.uniquelize('uniquelized/')
-    except FileNotFoundError:
-        print('File "unique_list.txt" not found')
+    videos = Videos(list(), list())
+
+    if len(args.source_file) != 0:
+        try:
+            with open(args.source_file) as file:
+                lines = filter(None, map(str.strip, file))
+                for line in lines:
+                    if is_url(line):
+                        videos.url.append(line)
+                    else:
+                        videos.path.append(line)
+            print(f'Parse videos from source [{args.source_file}] is completed :)')
+        except FileNotFoundError:
+            print(f'Source file [{args.source_file}] not found :(')
+
+    for video in args.videos:
+        if is_url(video):
+            videos.url.append(video)
+        else:
+            videos.path.append(video)
+
+    if len(videos.path) + len(videos.url) != 0:
+        uniq = VideoUniquelizer(path=videos.path, url=videos.url)
+        uniq.uniquelize(args.destination_dir)
+    else:
+        print('Script didn\'t get any videos :(')
 
 
 if __name__ == '__main__':
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,12 @@
+from dataclasses import dataclass
+
 from validators import url as is_valid_url
+
+
+@dataclass
+class Videos:
+    path: list[str]
+    url: list[str]
 
 
 def is_url(url: str) -> bool:
